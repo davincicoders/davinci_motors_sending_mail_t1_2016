d2fd5d5cd629f4f3c0fbd8c8ddadac56e2c3c785
Jason Noble <github+jasonn@jasonnoble.org>
2016-04-13 17:51:51 -0600

    End of class

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],
            scope: 'user:email'
+
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
